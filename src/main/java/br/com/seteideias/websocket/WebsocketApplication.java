377441630abf5639fd3005372cd7c7c0440aa20d
Cartaxo <carlos.pijanowski@gmail.com>
2024-01-25 19:57:34 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/websocket/WebsocketApplication.java
@@ -0,0 +1,68 @@
+package br.com.seteideias.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.NestedExceptionUtils;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.sql.Timestamp;
+
+import static java.time.LocalTime.now;
+
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+    record GrettingRequest(String name){}
+
+    record GreetingResponse(String message){}
+
+    @Controller
+    public class GreetingsWebSocketController{
+
+        @MessageExceptionHandler
+        @SendTo("/topic/errors")
+        String handleException(Exception e){
+            final var message = "Something went wrong processing request.: " + NestedExceptionUtils.getMostSpecificCause(e);
+            System.out.println(message);
+            return message;
+        }
+
+        @MessageMapping("/chat")
+        @SendTo("/topic/greetings")
+        GreetingResponse greet(GrettingRequest grettingRequest)throws Exception{
+            Assert.isTrue(Character.isUpperCase(grettingRequest.name().charAt(0)),() -> "the name must start with capital letter!");
+            Thread.sleep(1_000);
+            return new GreetingResponse("Hello, "+grettingRequest.name()+ " >> "+ now() +" - !");
+        }
+    }
+
+    @Configuration
+    @EnableWebSocketMessageBroker
+    class GreetingWebSockerConfiguration implements WebSocketMessageBrokerConfigurer{
+
+        @Override
+        public void configureMessageBroker(MessageBrokerRegistry registry) {
+            registry.enableSimpleBroker("/topic");
+            registry.setApplicationDestinationPrefixes("/app");
+        }
+
+        @Override
+        public void registerStompEndpoints(StompEndpointRegistry registry) {
+            registry.addEndpoint("/chat").withSockJS();
+        }
+    }
+
+}
