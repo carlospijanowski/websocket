cc888c1bd4a5779294e65050f47895909976146d
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2024-01-28 20:42:31 -0300

    atualizacoes

--- src/main/java/br/com/seteideias/websocket/WebsocketApplication.java
+++ src/main/java/br/com/seteideias/websocket/WebsocketApplication.java
@@ -1,8 +1,10 @@
 package br.com.seteideias.websocket;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.NestedExceptionUtils;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -11,12 +13,15 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import static java.time.LocalTime.now;
 
+@Slf4j
 @SpringBootApplication
 public class WebsocketApplication {
 
@@ -28,6 +33,7 @@ public class WebsocketApplication {
 
     record GreetingResponse(String message) {}
 
+    @CrossOrigin(origins = "*")
     @Controller
     public record GreetingsWebSocketController(
             SimpMessagingTemplate simpMessagingTemplate) {
@@ -44,8 +50,8 @@ public class WebsocketApplication {
         @SendTo("/topic/greetings")
         GreetingResponse greet(GrettingRequest grettingRequest) {
             Assert.isTrue(Character.isUpperCase(grettingRequest.name().charAt(0)), () -> "the name must start with capital letter!");
-//            Thread.sleep(1_000);
             System.out.println(">>>>>>>>>>>>>>>> " + grettingRequest.name());
+            System.out.println("HERE, UPDATE PROCESS TO DATABASE");
             return new GreetingResponse("Hello, " + grettingRequest.name() + " >> " + now() + " - !");
         }
     }
@@ -62,8 +68,14 @@ public class WebsocketApplication {
 
         @Override
         public void registerStompEndpoints(StompEndpointRegistry registry) {
-//            registry.addEndpoint("/chat");
+            registry.addEndpoint("/chat");
+            registry.addEndpoint("/chat").setAllowedOrigins("*");
             registry.addEndpoint("/chat").withSockJS();
+        }
+
+        @EventListener
+        public void onDisconnectEvent(SessionDisconnectEvent event){
+            log.info("Client with session id [{}] was disconnected",event.getSessionId());
         }
     }
 
